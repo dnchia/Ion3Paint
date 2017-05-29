5dad7099f6679dd0b9cc431abaef98dd35a8e96d
dnchia <daniel.chia.95@gmail.com>
2017-05-29 13:51:50 +0200

    Forced HTTPS

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
