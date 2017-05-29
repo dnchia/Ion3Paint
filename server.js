457310d29772142e0b0d7366b28af4d6f4567f68
dnchia <daniel.chia.95@gmail.com>
2017-05-29 13:47:30 +0200

    Included Procfile & modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 var express = require('express'),
   app = express();
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
